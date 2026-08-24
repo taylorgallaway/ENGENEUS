fb5cf948ed32c2a56b935bd337319bbd33962184
taylorgallaway <tgallaway1711@gmail.com>
2026-08-24 18:54:15 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="format-detection" content="telephone=no, date=no, address=no, email=no" />
+      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
