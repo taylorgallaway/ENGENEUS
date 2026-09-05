b6c5528c525d99e770944d4c4e290e34e8c4ed24
taylorgallaway <tgallaway1711@gmail.com>
2026-09-05 15:59:06 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,12 @@ export const metadata = {
   description: 'Learn Through Music',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
